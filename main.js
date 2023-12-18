3251c650c05b24eb9ef663cc9b80921f2c9a6248
Lionel EKOM <ekomlionel@gmail.com>
2023-12-18 13:05:30 +0100

    Made ressources

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: "John Doe",
+            age: 30,
+            email: "john@example.com",
+            image: "Assets\images\pamplemousse.jpeg"
+        }
+    }
+})
