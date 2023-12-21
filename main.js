d127ca2661a542e7e00f02fdfb40a2a6125fba8d
Lionel EKOM <ekomlionel@gmail.com>
2023-12-21 11:24:45 +0100

    Vue Events

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             name: "John Doe",
+            product: "Pamplemousse",
             age: 30,
+            price: 3000,
             email: "john@example.com",
-            image: "Assets\images\pamplemousse.jpeg"
+            image: "Assets/images/pamplemousse.jpeg",
+            count: 0
         }
     }
 })
